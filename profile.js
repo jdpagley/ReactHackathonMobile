0be723d5cfe8a6e805519e3f0066db39066982ae
Jeremy Pagley <jeremyjkp@gmail.com>
2016-04-19 19:05:48 -0400

    Profile Work

---
+++ profile.js
@@ -0,0 +1,108 @@
+import React, {
+  Component,
+  StyleSheet,
+  View,
+  ScrollView,
+  Image,
+  Text,
+  PropTypes
+} from 'react-native';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import Button from 'react-native-button/Button'
+import {Actions as RouterActions} from 'react-native-router-flux'
+import * as actionCreators from '../actions' 
+
+const styles = StyleSheet.create({
+  username: {
+    marginTop: 80,
+    fontSize: 30 
+  },
+  gif: {
+    width: 400,
+    height: 300
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scrollView: {
+    marginTop: 30,
+    height: 400
+  }
+});
+
+class Selector extends Component {
+
+  constructor (props) {
+    super(props);
+    this.getGifNodes = this.getGifNodes.bind(this);
+    this.handleShowLikes = this.handleShowLikes.bind(this);
+    this.handleShowDislikes = this.handleShowDislikes.bind(this);
+    this.state = {
+      active: 'liked'
+    };
+  }
+
+  render() {
+
+    const {user} = this.props;
+
+    console.log('user: ', this.state.active, user);
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.username}>{user.name}</Text>
+        <Button onPress={this.handleShowLikes}>
+          Likes 
+        </Button>
+        <Button onPress={this.handleShowDislikes}>
+          Disikes
+        </Button>
+        <ScrollView style={styles.scrollView}>
+          {this.getGifNodes(user[this.state.active])}
+        </ScrollView>
+      </View>
+    );
+
+  }
+
+  handleShowLikes() {
+    this.setState({active: 'liked'});
+  }
+
+  handleShowDislikes() {
+    this.setState({active: 'disliked'});
+  }
+
+  getGifNodes(gifs) {
+
+    if (!gifs) return <Text>No Gifs</Text>
+
+    return gifs.map((gif, i) => {
+      console.log('getGifNodes: ', gif, i);
+      return <Image key={i} source={{uri: gif}} style={styles.gif}/>
+    });
+
+  }
+}
+
+Selector.propTypes = {
+  user: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+  const {user} = state;
+  return {user};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actionCreators, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Selector)
